a1440692a90750eadac31fecf9953b17d7041522
gamja44 <https://github.com/gamja44/GDJ83_Java.git>
2024-12-11 19:37:45 +0900

    Test

--- src/spring/Example.java
+++ src/spring/Example.java
@@ -4,9 +4,8 @@ public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// test
-		int result = 1 + 1;
-		System.out.println("Test 성공");
+		
+		System.out.println("Java6_Class");
 		
 	}
 
